4d42a4df9de452f4c86fd12c93eb787d735a049e
Luneric <thaicarolyn8k@gmail.com>
2026-08-31 11:38:33 -0700

    fixed: divide by zero problem, close #61

--- main.cpp
+++ main.cpp
@@ -13,13 +13,19 @@ int main()
     int x,y;
 
     cin >> x >> y;
+
+    if (y == 0) {
+        cout << "Dividing by zero is not a number." << endl;
+    } else {
+        cout << "Division: " << x / y << endl;
+        cout << "Remainder: " << x % y << endl;
+    }
+
+    cout << "Square Root: " << sqrt(x) << endl;
+    cout << "Square: " << pow(x, y) << endl;
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
 
     return 0;
 }
